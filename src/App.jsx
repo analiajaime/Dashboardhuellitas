b73180db1bca3328e985d2a452e2ed88e26488a8
Analia Jaime <anjaime@yahoo.com.ar>
2024-03-24 22:31:15 -0300

    armado

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./componentes/ItemDetailContainer/ItemDetailContainer";
+import NavBar from "./componentes/NavBar/NavBar";
+import { CarritoProvider } from "./context/CarritoContext";
+import Cart from "./componentes/Cart/Cart";
+import Checkout from "./componentes/Checkout/Checkout";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <BrowserRouter>
+        <CarritoProvider>
+          <NavBar/>
+          <Routes>
+            <Route path="/" element={<ItemListContainer/>} />
+            <Route path="/categoria/:idCategoria" element={<ItemListContainer/>}  />
+            <Route path="/item/:idItem" element={<ItemDetailContainer/>}  />
+            <Route path="/cart" element={<Cart/>} />
+            <Route path="/checkout" element={<Checkout/>} />
+            <Route path="*" element={<h2>Sitio en construcción</h2>} />
+          </Routes>
+        </CarritoProvider>
+      </BrowserRouter>
+      
+
+      
+    </div>
   )
 }
-
-export default App
