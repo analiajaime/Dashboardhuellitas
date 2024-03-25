d2a14f66f4def9bcfd041cc2ea9263ee681b49fa
Analia Jaime <anjaime@yahoo.com.ar>
2024-03-25 12:47:29 -0300

    categorypanel

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import Footer from '../Footer/footer';
+import Footer from '../Footer/Footer';
 import DetailPanel from '../DetailPanel/DetailPanel';
 import ProductListPanel from '../ProductListPanel/ProductListPanel';
+import TotalPanel from '../TotalPanel/TotalPanel';
+import CategoryPanel from '../CategoryPanel/CategoryPanel';
 
 const Dashboard = () => {
   return (
@@ -10,8 +12,9 @@ const Dashboard = () => {
       <h1>Dashboard</h1>
       <ProductListPanel />
       <DetailPanel />
+      <CategoryPanel />
+      <TotalPanel />
       <Footer />
-      
     </div>
   );
 };
--- src/components/TotalPanel/TotalPanel.jsx
+++ src/components/TotalPanel/TotalPanel.jsx
@@ -1,26 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { getProducts } from '../../services/config';
+import React from 'react'
 
 const TotalPanel = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    getProducts()
-      .then((products) => setProducts(products))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
   return (
-    <div>
-      <h2>Products</h2>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    <div>TotalPanel</div>
+  )
 }
-export default TotalPanel;
 
-// Path: src/components/ProductsComponent/ProductsComponent.jsx
+export default TotalPanel
